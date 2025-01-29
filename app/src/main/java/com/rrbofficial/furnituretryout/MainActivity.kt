ff95772b25ac3e76a3f4b15053ad6798af249fe1
Rohit Balage <thatcomputerboy26@gmail.com>
2025-01-29 06:10:45 -0500

    Initial setup by adding sceneform and setting bottom sheet

---
+++ app/src/main/java/com/rrbofficial/furnituretryout/MainActivity.kt
@@ -0,0 +1,28 @@
+package com.rrbofficial.furnituretryout
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.TypedValue
+import com.google.android.material.bottomsheet.BottomSheetBehavior
+import kotlinx.android.synthetic.main.activity_main.*
+private const val BOTTOM_SHEET_PEEK_HEIGHT = 50f
+class MainActivity : AppCompatActivity() {
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        setupBottomSheet()
+    }
+
+
+    private fun setupBottomSheet()
+    {
+        val bottomSheetBehavior = BottomSheetBehavior.from(bottomSheet)
+         bottomSheetBehavior.peekHeight =
+             TypedValue.applyDimension(
+                 TypedValue.COMPLEX_UNIT_DIP,
+                 BOTTOM_SHEET_PEEK_HEIGHT,
+                 resources.displayMetrics).toInt()
+    }
+}
