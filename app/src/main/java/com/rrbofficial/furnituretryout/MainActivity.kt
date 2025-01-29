8afec83109e51b10cb31f2384ec28d9986ba0576
Rohit Balage <thatcomputerboy26@gmail.com>
2025-01-29 06:14:48 -0500

    fixed bottom sheet

--- app/src/main/java/com/rrbofficial/furnituretryout/MainActivity.kt
+++ app/src/main/java/com/rrbofficial/furnituretryout/MainActivity.kt
@@ -3,6 +3,7 @@ package com.rrbofficial.furnituretryout
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.TypedValue
+import android.view.View
 import com.google.android.material.bottomsheet.BottomSheetBehavior
 import kotlinx.android.synthetic.main.activity_main.*
 private const val BOTTOM_SHEET_PEEK_HEIGHT = 50f
@@ -24,5 +25,15 @@ class MainActivity : AppCompatActivity() {
                  TypedValue.COMPLEX_UNIT_DIP,
                  BOTTOM_SHEET_PEEK_HEIGHT,
                  resources.displayMetrics).toInt()
+        bottomSheetBehavior.addBottomSheetCallback(object : BottomSheetBehavior.BottomSheetCallback()
+        {
+            override fun onSlide(bottomSheet: View, slideOffset: Float) {
+                bottomSheet.bringToFront()
+            }
+
+            override fun onStateChanged(bottomSheet: View, newState: Int) {
+
+            }
+        })
     }
 }
